e709e7a27ec805a2c301fbeda833e6916f9dee5b
acrane <andrew.crane@dal.ca>
2017-04-04 12:17:32 -0300

    Refactored by 1 step

--- src/main/java/dal/gravity/PendulumRunner.java
+++ src/main/java/dal/gravity/PendulumRunner.java
@@ -14,10 +14,10 @@ public class PendulumRunner {
 
 	double delta = (args.length == 0) ? .1 : Double.parseDouble (args[0]);
 	double sLen = 10, pMass = 10, theta0 = Math.PI/30;
-	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, delta);
-	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0);
+	RegularPendulum rp = new RegularPendulum (sLen, pMass, theta0, delta, 9.80665);
+	SimplePendulum sp = new SimplePendulum (sLen, pMass, theta0, 9.80665);
 	RegularPendulum rpCoarse = 
-	    new RegularPendulum (sLen, pMass, theta0, .1);
+	    new RegularPendulum (sLen, pMass, theta0, .1, 9.80665);
 
 	// print out difference in displacement in 1 second intervals
 	// for 20 seconds
